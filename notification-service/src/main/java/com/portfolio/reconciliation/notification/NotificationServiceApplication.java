34bf99e9eb23a1ad070b07c6f8f0dad6dc45a4ea
vitaa1 <josevitorbarroso123@gmail.com>
2026-08-07 18:27:02 -0300

    feat(notification): NotificationProperties (from/to estaticos)

--- notification-service/src/main/java/com/portfolio/reconciliation/notification/NotificationServiceApplication.java
+++ notification-service/src/main/java/com/portfolio/reconciliation/notification/NotificationServiceApplication.java
@@ -1,9 +1,12 @@
 package com.portfolio.reconciliation.notification;
 
+import com.portfolio.reconciliation.notification.config.NotificationProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(NotificationProperties.class)
 public class NotificationServiceApplication {
   public static void main(String[] args) {
     SpringApplication.run(NotificationServiceApplication.class, args);
